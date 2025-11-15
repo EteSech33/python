03c892899c18c664cf67d51c2b0d20bb039e3ffe
Chacho <fp_gabrielkirilov@ciudadjardin.org>
2025-11-15 22:12:26 +0100

    Delete Funciones Básicas directory
    Se van a añadir archvios actualizados

--- Funciones Básicas/Ejercicio13.py
+++
@@ -1,5 +0,0 @@
-"""
-Ejercicio 13: División
-Escribir un programa que pida al usuario dos números y muestre por pantalla su división. 
-Si el divisor es cero el programa debe mostrar un error indicando “No es posible dividir entre 0”
-"""
