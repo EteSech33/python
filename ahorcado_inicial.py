d6c316915d550b09a0c7ef699d41afa4fa4606f4
Chacho <fp_gabrielkirilov@ciudadjardin.org>
2025-12-02 10:20:10 +0100

    Ahorcado mejorado

--- ahorcado_inicial.py
+++ ahorcado_inicial.py
@@ -56,38 +56,49 @@ AHORCADO = [
 numero = 0
 cont = 0
 adivinar = []
+palabra = []
 z = 0
 
-while (cont <= 6):
+# Elige una palabra de forma aleatoria y calcula su longitud y la convierte en rusta
+def get_palabra_secreta():
+      palabras = ["casa"] #,"coche","tomate","lechuga"
+      x = random.choice(palabras)
+      y = len(x)
+      palabra = list(x)
+      return x,y,palabra
 
-      letra = str(input("Introduce una letra para adivinar: "))
+x,y,palabra = get_palabra_secreta()
 
-      def get_palabra_secreta():
-            palabras = ["casa","coche","tomate","lechuga"]
-            x = random.choice(palabras)
-            y = len(x)
-            return x,y
+# Imprime la posicion de la horca que tenga el contador, si fallas el contador sube
+def print_horca(cont):
+      print(AHORCADO[cont])
 
-
-      def print_horca(numero):
-            print(AHORCADO[2])
-
-      x,y = get_palabra_secreta()
-
-      while (cont <= 6):
-            print(AHORCADO[cont])
-            cont = cont + 1
-
-      if cont >= 6:
-            print("Has muerto")
-            exit
-      
-      print(x,y)
-
+# Con la longitud de la palabra secreta hace una lista exacta de longitud pero con guiones
+def guiones(z,y):
       while (z != y):
             adivinar.append("-")
-            z = z + 1             
+            z = z + 1 
+guiones(z,y)
+
+# Mientras que el contador sea menor que 6 se hace todo
+while (cont <= 6):
+
+      print_horca(cont)
+
+      letra_usuario = str(input("Introduce una letra para adivinar: "))
+      pos=0
+      for letra in palabra: #Por cada letra en la palabra secreta, comprueba que si la letra del usuario es la letra de la palabra secreta
+            if letra_usuario == letra:
+                  adivinar[pos]=letra_usuario 
+            pos+=1
+
+      if letra_usuario not in palabra:    # SI la letra no esta dentro aumenta el contador
+            cont+=1
       
+      if adivinar == palabra:
+            print("Has ganado!")
+            break
+
       print(adivinar)
 
 
