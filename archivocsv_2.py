4ba4e6f9ed23f93b2411fa896200b91ec6eec2a2
Chacho <fp_gabrielkirilov@ciudadjardin.org>
2026-01-14 12:41:44 +0100

    cositas lindas de csv

--- archivocsv_2.py
+++ archivocsv_2.py
@@ -13,4 +13,20 @@ for pelicula in lista:
 
 # Escribir linea en el csv con DictWriter
 fields=["Titulo","Genero","Fecha","Nota"]
-new_film=['Titulo':'El Resplandor','Genero':'Terror','Fecha':'1998','Nota':'10']
+new_film={'Titulo':'El Resplandor','Genero':'Terror','Fecha':'1998','Nota':'10'}
+with open("peliculas.csv","a",newline="") as peliculas:
+    writer = csv.DictWriter(peliculas,fields)
+    writer.writerow(new_film)
+
+
+# Escribir un csv nuevo
+fields2 = ["Marca","Modelo","Precio"]
+coche1 = {'Marca':'Tesla','Modelo':'S','Precio':'5'}
+coche2 = {'Marca':'BMW','Modelo':'M3','Precio':'104000'}
+coche3 = {'Marca':'Polloneta','Modelo':'Pollo','Precio':'29323'}
+coches = [coche1, coche2, coche3]
+
+with open("concesionario.csv", "w", newline="") as concesionario:
+    writer = csv.DictWriter(concesionario,fields2)
+    writer.writeheader()
+    writer.writerows(coches)
